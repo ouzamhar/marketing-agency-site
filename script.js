4b049a916ceeec5b8db4908f09efc1f4b15fc1c4
ouzamhar <58828332+ouzamhar@users.noreply.github.com>
2022-08-13 16:08:02 +0100

    fix home scroll

--- script.js
+++ script.js
@@ -1,9 +1,13 @@
 const closeButton = document.querySelector(".close-nav");
 const openButton = document.querySelector(".open-nav");
 const nav = document.querySelector(".nav");
+const body = document.querySelector("body");
 closeButton.addEventListener("click", () => {
   nav.classList.remove("navigation-open");
+  body.classList.remove("no-scroll");
 });
 openButton.addEventListener("click", () => {
   nav.classList.add("navigation-open");
+  body.classList.add("no-scroll");
+  console.log(body);
 });
