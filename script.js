b8fdeb3f7f53657848f9148b0f60e309149280c4
ouzamhar <58828332+ouzamhar@users.noreply.github.com>
2022-08-14 11:10:55 +0100

    fix page scroll on mobile menue

--- script.js
+++ script.js
@@ -1,11 +1,13 @@
 const closeButton = document.querySelector(".close-nav");
 const openButton = document.querySelector(".open-nav");
 const nav = document.querySelector(".nav");
-
+const body = document.querySelector("footer");
 closeButton.addEventListener("click", () => {
   nav.classList.remove("navigation-open");
+  document.documentElement.style.overflowY = "";
 });
-
 openButton.addEventListener("click", () => {
   nav.classList.add("navigation-open");
+  body.classList.add("no-scroll");
+  document.documentElement.style.overflowY = "hidden";
 });
